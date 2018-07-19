3b3fa4c424af2e2abfd89d09dc949ce426bb60c9
Nell Hardcastle <nell@squishymedia.com>
2018-07-19 13:10:16 -0700

    Add the content configuration tree.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Index of content to build
+import faq from './components/faq.js'
+import frontPage from './components/frontPage.js'
+import theme from './theme.json'
+
+export default {
+  frontPage,
+  faq,
+  theme
+}
